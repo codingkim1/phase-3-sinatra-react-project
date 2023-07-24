2a15cba1329b6b9447df934edf42ad2da6920ab0
Alex <alexkimanthi92@gmail.com>
2023-07-24 22:26:03 +0300

    updated controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,35 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/messages" do
+    message = Message.all 
+    message.to_json 
+  end
+
+  post '/messages' do 
+    message = Message.create(
+      body: params[:body],
+      username: params[:username],
+      created_at: params[:created_at] 
+    )
+    message.to_json 
+  end
+
+  patch '/messages/:id' do 
+    message = Message.find(params[:id])
+    message.update(
+      body: params[:body]
+    )
+    message.to_json
+  end
+
+  delete '/messages' do 
+    Message.all.destroy 
+  end
+
+  delete '/messages/:id' do 
+    message = Message.find(params[:id])
+    message.destroy 
   end
 
 end
