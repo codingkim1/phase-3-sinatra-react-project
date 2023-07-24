1aabd1657d1f86d3d01d784ce6c13dc58b89b8ff
Alex <alexkimanthi92@gmail.com>
2023-07-24 22:18:14 +0300

    created seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,20 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
+Message.create([
+    {
+      body: "Success is not final; failure is not fatal: It is the courage to continue that counts.",
+      username: "Winston S. Churchill"
+    },
+    {
+      body: "It is better to fail in originality than to succeed in imitation.",
+      username: "Herman Melville"
+    },
+    {
+      body: "The road to success and the road to failure are almost exactly the same.",
+      username: " Colin R. Davis"
+    }
+   
+  ])
+
 puts "✅ Done seeding!"
