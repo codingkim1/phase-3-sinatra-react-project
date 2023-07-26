531c9af278674c77db00ce029361255d21f3f1c1
Alex <alexkimanthi92@gmail.com>
2023-07-26 11:08:13 +0300

    created migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_26_073948) do
+ActiveRecord::Schema.define(version: 2023_07_26_080449) do
 
   create_table "messages", force: :cascade do |t|
     t.string "body"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2023_07_26_073948) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "YoB"
   end
 
 end
